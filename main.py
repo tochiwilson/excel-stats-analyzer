89c922e830550f56f6ea8fc0fb3778a2bcc54c60
Wilson Tochi Uduma <tochiwilson10@gmail.com>
2025-01-25 21:02:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+# Load an Excel File and Get Basic Stats
+# Description:
+# Create a script that loads an Excel file and calculates statistics
+# such as the mean, sum, maximum, and minimum of numerical columns.
+# Provide an overview of column names, the number of rows, and the number of missing values per column.
+#
+# Challenge:
+# Add data visualizations. Use the matplotlib or pandas library to generate graphs,
+# such as histograms for numerical columns or a boxplot to visualize the spread.
+# This helps make the statistics more insightful.
+
+
